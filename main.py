3ea45e7648fc2fe659bb37c3a78a9a51390ba749
danilborisov <bordanchannel@gmail.com>
2023-08-23 20:34:37 +1200

    Solute issues and I think, thats first alfa ver1.0

--- main.py
+++ main.py
@@ -10,8 +10,23 @@ TOKEN = '6692357135:AAGj7Fs2tD8Gah-Bcnj1JMUAVkrFgdGzPa0'
 bot = telebot.TeleBot(TOKEN)
 
 
+keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+forecast_button = telebot.types.KeyboardButton("Forecasts")
+keyboard.add(forecast_button)
+
+
 @bot.message_handler(commands=['start'])
 def start(message):
+    chat_id = message.chat.id
+    random_key = random.choice(list(funny_forecasts.keys()))
+    random_value = funny_forecasts[random_key]
+    bot.send_message(chat_id, f'Hi, I\'m a bot that can provide you with funny forecasts!')
+    bot.send_message(chat_id, f'Here are some things you can do:')
+    bot.send_message(chat_id, f'- Press the "Forecasts" button below to get a random forecast.', reply_markup=keyboard)
+
+
+@bot.message_handler(func=lambda message: message.text.lower() == "forecasts")
+def send_forecast(message):
     chat_id = message.chat.id
     random_key = random.choice(list(funny_forecasts.keys()))
     random_value = funny_forecasts[random_key]
@@ -22,4 +37,5 @@ def start(message):
         bot.send_message(chat_id, "Your message there.")
 
 
+# This line starts the bot polling for updates
 bot.polling()
