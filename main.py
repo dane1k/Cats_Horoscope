788032b99b6938de9f9ebd606d85a125f64d5778
danilborisov <bordanchannel@gmail.com>
2023-08-25 10:18:26 +1200

    PredFinalyUpdate

--- main.py
+++ main.py
@@ -1,28 +1,27 @@
 # SQLkiss Cat's Horoscope
-import datetime
 import random
 import time
 import telebot
 from Zodiac_Signs import funny_forecasts
  
 #bot token 
-TOKEN = '6692357135:AAGj7Fs2tD8Gah-Bcnj1JMUAVkrFgdGzPa0'
+TOKEN = 'BOTTELEGRAMTOKEN' # YOUR TOKEN
 bot = telebot.TeleBot(TOKEN)
-group_chat_id = 'Cats_Horoscope'
+group_chat_id = '-1001760424253' #
+                                 #CHAT GROUP ID: how to find this:
+                                 #  https://stackoverflow.com/questions/32423837/telegram-bot-how-to-get-a-group-chat-id
+                                 # Choose the answer with 58's rating ('the simplest way i found using only telegram-web :')
+                                 #
 
 
 keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
 forecast_button = telebot.types.KeyboardButton("Forecasts")
 keyboard.add(forecast_button)
 
-keyboard2 = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
-post_button = telebot.types.KeyboardButton("/post")
-keyboard.add(post_button)
-
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    chat_id = message.chat.id
+    chat_id =  message.chat.id
     random_key = random.choice(list(funny_forecasts.keys()))
     random_value = funny_forecasts[random_key]
     bot.send_message(chat_id, f'Hi, I\'m a bot that can provide you with funny forecasts!')
@@ -30,16 +29,9 @@ def start(message):
     bot.send_message(chat_id, f'- Press the "Forecasts" button below to get a random forecast.', reply_markup=keyboard)
 
 
-@bot.message_handler(commands=['post'])
-def post_message(message):
-    chat_id = message.chat.id
-    forecast_result = send_forecast(message)
-    bot.send_message(chat_id, forecast_result)
-
-
 @bot.message_handler(func=lambda message: message.text.lower() == "forecasts")
 def send_forecast(message):
-    chat_id = message.chat.id
+    chat_id = group_chat_id
     random_key = random.choice(list(funny_forecasts.keys()))
     random_value = funny_forecasts[random_key]
     bot.send_message(chat_id, f'{random_value}')
