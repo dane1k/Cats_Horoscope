3a05a832667aedb6c8b83ea27f671155ab7eb977
dane1k <dan9.b0rdan@yandex.ru>
2023-08-29 00:07:02 +1200

    new uopdate main and requirements

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 # SQLkiss Cat's Horoscope
 import random
 import telebot
-import schedule
 import sys
 import os
 from Zodiac_Signs import funny_forecasts
- 
+
 #bot token 
 TOKEN = os.environ.get('BOT_TELEGRAM_TOKEN_FOR_CODE', 'nothing')
 if TOKEN == 'nothing':
     sys.exit("TOKEN not accept")
-    
+
 bot = telebot.TeleBot(TOKEN)
 group_chat_id = '-1001760424253' #
                                  #CHAT GROUP ID: how to find this:
@@ -19,17 +18,13 @@ group_chat_id = '-1001760424253' #
                                  #
 
 
-keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
-forecast_button = telebot.types.KeyboardButton("Forecasts")
-keyboard.add(forecast_button)
-
-
 # fun for checking and updating work of bot
 @bot.message_handler(commands=['start'])
 def start(message):
     chat_id =  message.chat.id
-    random_key = random.choice(list(funny_forecasts.keys()))
-    random_value = funny_forecasts[random_key]
+    keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+    forecast_button = telebot.types.KeyboardButton("Forecasts")
+    keyboard.add(forecast_button)
     bot.send_message(chat_id, f'Hi, I\'m a bot that can provide you with funny forecasts!')
     bot.send_message(chat_id, f'Here are some things you can do:')
     bot.send_message(chat_id, f'- Press the "Forecasts" button below to get a random forecast.', reply_markup=keyboard)
@@ -44,7 +39,6 @@ def send_forecast(message):
     random_key = random.choice(list(funny_forecasts.keys()))
     random_value = funny_forecasts[random_key]
     bot.send_message(chat_id, f'{random_value}')
-
     return f"Thank you for reading our horoscope"
 
 
@@ -54,7 +48,6 @@ def post_forecast():
     send_forecast(chat_id)
 
 
-schedule.every().day.at("12:49").do(post_forecast)
 # running script
 if __name__ == "__main__":
     post_forecast()
