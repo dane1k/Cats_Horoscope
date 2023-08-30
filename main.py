2b67763b9c5637eac60529c48bee915bea461000
danilborisov <bordanchannel@gmail.com>
2023-08-30 18:29:54 +1200

    Update

--- main.py
+++ main.py
@@ -11,30 +11,28 @@ openai.api_key = os.environ.get('OPENAI_API_KEY', 'notapi')
 if openai == 'notapi':
     sys.exit("API Key not accept")
 
-#bot token 
+# BOT Token
 TOKEN = os.environ.get('BOT_TELEGRAM_TOKEN_FOR_CODE', 'nothing')
 if TOKEN == 'nothing':
     sys.exit("TOKEN not accept")
-
-
 bot = telebot.TeleBot(TOKEN)
 group_chat_id = '-1001760424253' #
-                                 #CHAT GROUP ID: how to find this:
+                                 # CHAT GROUP ID: how to find this:
                                  #  https://stackoverflow.com/questions/32423837/telegram-bot-how-to-get-a-group-chat-id
                                  # Choose the answer with 58's rating ('the simplest way i found using only telegram-web :')
                                  #
 
-
+# Question for the chatGPT *prompt* and choose random funny forecast
 random_value = random.choice(list(funny_forecasts.values()))
 prompt = f"Rephrase the following sentence, making it sound different but retaining the same meaning and add emoji:\n{random_value}"
 response = openai.Completion.create(
-    engine="davinci",
+    engine="davinci", # Model
     prompt=prompt,
     max_tokens=100
 )
-chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": f"{prompt}"}])
+chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": f"{prompt}"}]) #creating discussion
 
-# fun for checking and updating work of bot
+# fun for checking and updating work of bot into privat messages *this is just for self-debugging*
 @bot.message_handler(commands=['start'])
 def start(message):
     chat_id =  message.chat.id
@@ -46,17 +44,12 @@ def start(message):
     bot.send_message(chat_id, f'- Press the "Forecasts" button below to get a random forecast.', reply_markup=keyboard)
 
 
-# fun for getting forecasts from dictionary (from Zodiac_Sighns)
+# fun for getting forecasts
 @bot.message_handler(func=lambda message: message.text.lower() == "forecasts")
 def send_forecast(message):
-    global random_value
-    global random_key
     chat_id = group_chat_id
-    random_key = random.choice(list(funny_forecasts.keys()))
-    random_value = funny_forecasts[random_key]
     bot.send_message(chat_id, chat_completion.choices[0].message.content)
     return f"Thank you for reading our horoscope"
-
 
 # fun for posting the forecasts to group
 def post_forecast():
